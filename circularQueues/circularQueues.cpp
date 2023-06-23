68754a8af8598103115a2e5f085c6346356e50f3
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-06-23 11:29:17 +0700

    program selesai

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -76,7 +76,7 @@ public:
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
 				cout << queue_array[FRONT_position] << "   ";
-				FRONT_position;
+				FRONT_position++;
 			}
 			cout << endl;
 		}
