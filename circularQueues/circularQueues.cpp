3b0fda31efc4c6c0b11e035dbc995155c556b7e4
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-06-23 09:30:40 +0700

    mengecek apakah antrian kosong

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -17,10 +17,19 @@ public:
 		cin >> num;
 		cout << endl;
 
-		//cek apakah antria penuh
+		//cek apakah antrian penuh
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
+		}
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+
 		}
 	}
 };
